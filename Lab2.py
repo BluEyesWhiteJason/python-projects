215b50b95759a606e13222684a0ad4f596253301
BluEyesWhiteJason <jason.speet@gmail.com>
2019-07-16 13:32:02 -0400

    working lab 2

---
+++ Lab2.py
@@ -0,0 +1,9 @@
+
+width = float(input("Please enter width of room: "))
+
+length = float(input("Please enter length of room: "))
+
+area = width * length
+perimeter = width * 2 + length * 2
+
+print("The area is {} and the perimiter is {}".format(area, perimeter))
