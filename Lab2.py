42e53feceddcb59b99cd5dc2f5937663e9da6eda
BluEyesWhiteJason <jason.speet@gmail.com>
2019-07-16 13:40:07 -0400

    added volume

--- Lab2.py
+++ Lab2.py
@@ -3,7 +3,10 @@ width = float(input("Please enter width of room: "))
 
 length = float(input("Please enter length of room: "))
 
+height = float(input("Please enter height of room: "))
+
 area = width * length
 perimeter = width * 2 + length * 2
+volume = width * length * height
 
-print("The area is {} and the perimiter is {}".format(area, perimeter))
+print("The area is {} and the perimiter is {} and the volume is {}".format(area, perimeter, volume))
